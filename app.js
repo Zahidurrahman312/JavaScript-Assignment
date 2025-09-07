7de7852a0b18cc732f18fcd23d783ab88f3c04f7
unknown <zahidrehamn886554@gmail.com>
2025-09-08 02:48:43 +0500

    arry

---
+++ app.js
@@ -0,0 +1,55 @@
+console.log("Jvavascript Conected!");
+
+// ____________Q-01_____________________
+
+// var student = [];
+
+// student [0] ="Ali";
+// console.log(student);
+
+// _______Q-02________________________
+
+// var stringArry = [ "Apple","orang","banana"]
+
+// console.log(stringArry);
+
+// _________________Q-03_________________
+
+// var numberArry = [23,20 ,10, 40, 50];
+
+// console.log(numberArry);
+
+// ________________Q-04_________________
+
+// var boleanArry = [ true,false,]
+
+//     console.log(boleanArry);
+    
+// ________________Q-05________________
+
+// var mixArry = [ "apple",24,10,true,]
+// console.log(mixArry);
+
+// __________________Q-06_______________
+
+// var education = ["SSC","HSC","BSC","BS","BCOM","MS","M.PhiL","Ph.D"]
+
+// document.write("1) "+ education[0]+  "<br />"+"2) " + education[1]+  "<br />" + "3) "+ education[2] + "<br />" + "4) "+ education[3]+ "<br />" + "5) " + education[4] + "<br />"+ "6) " + education[5] + "<br />" + "7) " + education[6] + "<br />" + "8) "  + education[7] );
+
+// ________________Q-07________________
+
+// var studentName = ["Ahmad","Ali","Bilal"];
+
+// var obtainMarks = [320,220,480];
+
+// var totalMarks = [500];
+
+// for(var i =0; i < studentName.length; i++ ){
+//     var percent =(obtainMarks[i] / totalMarks)*100;    
+
+//     }
+//     document.write("Score of "+ studentName[i]+ " is "  )
+
+
+
+
