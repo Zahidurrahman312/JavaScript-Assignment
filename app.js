93d2cf895bf44ad947ca746a8a86af8fcd83f031
unknown <zahidrehamn886554@gmail.com>
2025-09-10 18:15:37 +0500

    80% compelte

--- app.js
+++ app.js
@@ -49,6 +49,173 @@ console.log("Jvavascript Conected!");
 
 //     }
 //     document.write("Score of "+ studentName[i]+ " is "  )
+// 
+// _______________________Q-08_____________________________
+
+// var studentName = ["Michael","Jhon","Tony"];
+
+// var score = [320,230,480];
+
+// var totalMarks = [500]
+// document.write("Score of Michael is "+ score[0]+ " Percentage: " + (320/500*100)+"%" +"<br />");
+// document.write("Score of Jhon is "+ score[1] + " Percentage: " + (230/500*100)+"%" +"<br />");
+
+// document.write("Score of Tony is "+ score[2]+ " Percentage: "+ (480/500*100)+"%" + "<br />" );
+
+// _____________________Q-09__________________________
+
+// var color = ["red","blue","orange","yellow"];
+
+// // document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3])
+
+// var user = prompt("Enter your favorite color");
+
+// color.unshift("brown");
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3] + "<br />"+ color[4] )
+
+// _____________part(b)_____________
+
+// var color = ["red","blue","orange","yellow"];
+
+// var user = prompt("Enter your favorite color");
+
+// color.push("Green");
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3] + "<br />"+ color[4] )
+
+// ____________________part(c)__________________
+
+// var color = ["red","blue","orange","yellow"];
+
+// color.unshift("Green","light Blue");
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3] + "<br />"+ color[4]+"<br />" +color[5] );
+
+// ________________part(d)_________________________
+
+// var color = ["red","blue","orange","yellow"];
+
+// color.shift();
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2] );
+
+// _____________________part(e)___________________________
+
+// var color = ["red","blue","orange","yellow"];
+ 
+// color.pop();
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2] );
+
+// ___________-__________part(f)___________________________
+
+// var color = ["red","blue","orange","yellow"];
+
+// color.splice(2,0,"Green","brown");
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3]+ "<br />" + color[4] );
+
+// // syntax of splice ==> (deletecount,add index ,items)
+
+// _________________________part(g)_____________________________
+
+// var color = ["red","blue","orange","yellow"];
+
+// color.splice(0,1,"pink","white");
+
+// document.write(color[0]+ "<br />"+ color[1]+"<br />"+color[2]+"<br />"+color[3]+ "<br />" +color[4] );
+
+// ______________________Q-10____________________________________
+
+// var score = [320,230,480,120];
+
+// score.sort();
+
+// console.log(score);
+
+// _______________________Q-11__________________________________
+
+// var cities = ["karachi","lahore","peshawer","quetta","Islamabad"];
+
+//     console.log(cities);
+    
+// var selectCities =cities.slice(0,2)
+
+// console.log(selectCities);
+
+// ________________________________Q-12__________________________________
+
+// var arry = ["This","is ","is","my","cat"];
+
+// console.log(arry);
+
+// console.log("This " + " is " + " my "+" cat ");
+
+// _____________________________Q-13_________________________________________
+
+// var emptyArray = [];
+
+// emptyArray[0]="keyboard";
+
+// emptyArray[1]="mouse";
+
+// emptyArray[2]="printer";
+
+// emptyArray[3]="monitor";
+// console.log(emptyArray);
+
+// for(var i =emptyArray.length-1;i>=0;i--){
+//     console.log(emptyArray[i]);
+    
+// }
+
+// _____________________Q-14____________________________
+
+// var sales = [];
+
+// sales[0]= "keyboard";
+
+// sales[1]= "mouse";
+
+// sales[2]= "printer";
+
+// sales[3]="monitor";
+// console.log(sales);
+
+// // we use for loop :
+
+// for(var i = 0; i<sales.length; i++){
+
+//     console.log(sales[i]);
+    
+// }
+
+// ___________________Q-15___________________
+
+// var manufacture =["apple","smsung","nokia","matrola","sony","haier"];
+
+// document.write("<select>");
+
+// for(var i = 0; i<manufacture.length; i++){
+
+//     document.write(" <option>" + manufacture[i] + "</select>");
+// }
+
+// document.write("</select>");
+
+// var user = prompt("Type any 65 to 122");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
