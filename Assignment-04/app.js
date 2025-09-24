dbf55a41e3abe1e97540f9b39d3af2feda0792c4
Zahidurrahman312 <zahidrehman886554@gmail.com>
2025-09-25 00:17:38 +0500

    Add files via upload

---
+++ Assignment-04/app.js
@@ -0,0 +1,87 @@
+console.log("Javascript connected!");
+
+//____________________Q-01____________________
+
+// var emptyArrary = [
+
+//     [],
+//     [],
+//     [],
+//     [],
+
+// ];
+
+// //_____________________Q-02____________________
+
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1],
+// ];
+
+// for(var i = 0; i < matrix.length; i++){
+
+//     var table = " ";
+    
+//     for(var j = 0; j < matrix[i].length; j++){
+
+//         table +=(matrix[i][j]) + " "
+        
+
+//     }
+
+//     console.log(table);
+    
+// };
+
+//_____________________________Q-03____________________________
+
+// for(var i =1; i <=10; i++){
+
+
+//     console.log(i);
+    
+// }
+
+//________________________Q-04___________________________________
+
+// var input =+prompt("Enter any number do you want multiply");
+
+// var userInput = +prompt("How many time do you want multiply");
+
+// for(var i = 1 ; i <= userInput; i++){
+
+    
+//             document.write(input + " x "+ i + " = " + (input*i) + "<br />");
+
+
+
+
+// };
+
+//_______________________________Q-05________________________________
+
+
+// var fruit = ["apple","banana","mango","straberry","orange"];
+
+// for(var i = 0 ; i < fruit.length; i++){
+
+//     document.write(fruit[i] + "<br />")
+// }
+
+//______________________________________Q-06_______________________________
+
+for(var i = 1; i <= 15; i++){
+
+    document.write(i);
+
+    if(i <15){
+
+        document.write(" , ");
+        
+    }
+    
+}
+
+
+
